5efcd6e4438b362971dd30334064cc10aa7ff984
Julia <julgol2002@gmail.com>
2024-10-27 13:47:47 +0100

    fetch api

--- src/models/User.php
+++ src/models/User.php
@@ -1,17 +1,31 @@
 <?php
 
 class User {
+    private $id;
     private $email;
     private $password;
+    private $salt;
+    private $name;
+    private $surname;
+    private $phone;
 
-    public function __construct(string $email, string $password){
+    public function __construct($id, $email, $password, $salt, $name, $surname, $phone) {
+        $this->id = $id;
         $this->email = $email;
         $this->password = $password;
+        $this->salt = $salt;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->phone = $phone;
     }
 
     public function getEmail() { return $this->email; }
-    public function setEmail($email) { $this->email = $email; }
     public function getPassword() { return $this->password; }
-    public function setPassword($password) { $this->password = $password; }
+    public function getId(): int {  return $this->id; }
+    public function getSalt(): string {  return $this->salt; }
+    public function getName() { return $this->name; }
+    public function getSurname() { return $this->surname; }
+    public function getPhone() { return $this->phone; }
+
 
 }
--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -6,22 +6,49 @@ require_once __DIR__.'/../models/User.php';
 class UserRepository extends Repository {
     public function getUser(string $email): ?User {
         $stmt = $this->database->connect()->prepare('
-            SELECT * FROM public.users WHERE email = :email
-        ');
+        SELECT * FROM users_details WHERE email = :email
+    ');
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
         $stmt->execute();
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($user == false) {
-            return null;
-        }
+        if ($user == false) return null;
 
         return new User(
+            $user['id'],
             $user['email'],
             $user['password'],
+            $user['salt'],
             $user['name'],
-            $user['surname']
+            $user['surname'],
+            $user['phone']
         );
+    }
+
+    public function createUser(string $email, string $password, string $salt, string $name, string $surname, string $phone)  {
+        $pdo = $this->database->connect();
+        $pdo->beginTransaction();
+        $pdo->exec('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE');
+
+        try {
+            $stmt = $this->database->connect()->prepare('
+                INSERT INTO user_details (name, surname, phone) 
+                VALUES (?, ?, ?) 
+                RETURNING id
+            ');
+            $stmt->execute([$name, $surname ,$phone]);
+            $userDetailsId = $stmt->fetchColumn();
+
+            $stmt = $this->database->connect()->prepare('
+                INSERT INTO users (iddetails, email, password, salt) 
+                VALUES (?, ?, ?, ?)
+            ');
+            $stmt->execute([$userDetailsId, $email, $password, $salt]);
+
+            $pdo->commit();
+        } catch (Exception $e) {
+            $pdo->rollBack();
+        }
     }
 }
